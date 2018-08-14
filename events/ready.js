7f6f613109e6fb8af961ac62d4d93319a9b07893
Kenneth Anderson <eucliwood98@gmail.com>
2018-08-14 18:58:23 +0800

    Changed from displaying how many guild the bot is in, to how many users

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,13 @@
 module.exports = client => {
 	console.log(`Logged in as ${client.user.tag}!`);
 	const config = client.defaultSettings;
-	client.emojiList = client.guilds.get('299841356942278656').emojis;
-	client.user.setActivity(`with ${client.guilds.size} bunnies! | type ${config.prefix}help`);
+	try {
+		client.emojiList = client.guilds.get('299841356942278656').emojis;
+	}
+	catch(err) {
+		console.log(err);
+	}
+	console.log(client.users);
+	client.user.setActivity(`with ${client.users.size} Masters! | type ${config.prefix}help`);
 	client.dataAutoUpdate();
 };
